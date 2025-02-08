755efc51fcd96e43996702512c606b5ad81ed002
Enner Mendizabal <159963896+Ennero@users.noreply.github.com>
2025-02-07 22:31:57 -0600

    FINALIZADito

--- SimulacionListas_MIA/src/main/java/com/mycompany/simulacionlistas_mia/SimulacionListas_MIA.java
+++ SimulacionListas_MIA/src/main/java/com/mycompany/simulacionlistas_mia/SimulacionListas_MIA.java
@@ -19,33 +19,52 @@ public class SimulacionListas_MIA {
 
     //Función para generar una lista contigua
     public void listaContigua() {
+        boolean cancelar = false; //para manejar la cancelación de la operación
         int n, i;
         int[] lista = new int[10]; //Creo un arreglo de tamañao de 10
         String entrada = JOptionPane.showInputDialog("Ingrese el número de elementos (el máximo es 10)");
-        n = Integer.parseInt(entrada);
+        if (entrada == null || "".equals(entrada)) {
+            JOptionPane.showMessageDialog(null, "Operación cancelada");
+            cancelar = true;
+        }
 
-        if (n > 10) {//Si es muy grande pues error xd
-            JOptionPane.showMessageDialog(null, "Se ingresó un número mayor a 10, no se puede continuar con la acción");
-        } else {//Si no continuamos con el proceso de crear la lista
-            for (i = 0; i < n; i++) {//Ciclo for que solicita los elementos de la lista
-                String valorcito = JOptionPane.showInputDialog("Ingrese el elemento " + i + ":");
-                lista[i] = Integer.parseInt(valorcito);
-            }
-            String textofinal = "[";
-            for (i = 0; i < n; i++) {
-                if (i == 0) {
-                    textofinal += String.valueOf(lista[i]);
-                } else {
-                    textofinal += ", " + String.valueOf(lista[i]);
+        if (!cancelar) {
+            n = Integer.parseInt(entrada);
+
+            if (n > 10) {//Si es muy grande pues error xd
+                JOptionPane.showMessageDialog(null, "Se ingresó un número mayor a 10, no se puede continuar con la acción");
+            } else {//Si no continuamos con el proceso de crear la lista
+                for (i = 0; i < n; i++) {//Ciclo for que solicita los elementos de la lista
+                    String valorcito = JOptionPane.showInputDialog("Ingrese el elemento " + i + ":");
+                    if (valorcito == null) {
+                        JOptionPane.showMessageDialog(null, "Operación cancelada");
+                        cancelar = true;
+                        break;
+                    }
+                    lista[i] = Integer.parseInt(valorcito);
+                }
+
+                if (!cancelar) {
+                    String textofinal = "[";
+                    for (i = 0; i < n; i++) {
+                        if (i == 0) {
+                            textofinal += String.valueOf(lista[i]);
+                        } else {
+                            textofinal += ", " + String.valueOf(lista[i]);
+                        }
+                    }
+                    textofinal += "]";
+                    JOptionPane.showMessageDialog(null, "La lista que se ingresó es:\n" + textofinal);
                 }
             }
-            textofinal += "]";
-            JOptionPane.showMessageDialog(null, "La lista que se ingresó es:\n" + textofinal);
+
         }
+
     }
 
     //Función para generar la lista ligada
     public void listaLigada() {
+        boolean cancelar = false; //para manejar la cancelación de la operación
         int cabeza, actual, fin;
 
         int[][] nodo = new int[10][2]; //Matriz para almacenar todo
@@ -58,6 +77,13 @@ public class SimulacionListas_MIA {
         while (actual < 10 && fin == 0) {
             //Aquí comienzo a solicitar que ingrese la cantidad de elementos que desee
             String valor = JOptionPane.showInputDialog("Ingrese un elemento (ingrese -1 para terminar)");
+
+            if (valor == null || "".equals(valor)) {
+                JOptionPane.showMessageDialog(null, "Operación cancelada");
+                cancelar = true;
+                break;
+            }
+
             int valorito = Integer.parseInt(valor);
             if (valorito == -1) {
                 fin = 1;
@@ -68,28 +94,138 @@ public class SimulacionListas_MIA {
                 actual = actual + 1;
             }
         }
-        //Ciclo para ir recorriendo la lista
-        String textofinal = "[";
-        actual = cabeza;
-        boolean primero = true; //bandera para que se muestre bonito
+        if (!cancelar) {
+            //Ciclo para ir recorriendo la lista
+            String textofinal = "[";
+            actual = cabeza;
+            boolean primero = true; //bandera para que se muestre bonito
 
-        while (actual != -1) {
-            if (primero) {
-                textofinal += nodo[actual][0];
-                primero = false;
-            } else {
-                textofinal += ", " + nodo[actual][0];
+            while (actual != -1) {
+                if (primero) {
+                    textofinal += nodo[actual][0];
+                    primero = false;
+                } else {
+                    textofinal += ", " + nodo[actual][0];
+                }
+                actual = nodo[actual][1]; //Me muevo al siguiente nodo
             }
-            actual = nodo[actual][1]; //Me muevo al siguiente nodo
+            textofinal += "]"; //Cierro todo
+            JOptionPane.showMessageDialog(null, "La lista que se ingresó es:\n" + textofinal);
         }
-        textofinal += "]"; //Cierro todo
-        JOptionPane.showMessageDialog(null, "La lista que se ingresó es:\n" + textofinal);
     }
 
     //Función para generar la lista doblemente ligada
+    //Es basicamente la seguna pero reacondicionada con el doble apuntado
+    public void listaDoblementeLigada() {
+        boolean cancelar = false; //para manejar la cancelación de la operación
+        int cabeza, actual, previo, fin;
 
+        int[][] nodo = new int[10][3]; //Matriz para almacenar todo
+        //Asigno valores iniciales para la lista enlazada simple
+        cabeza = -1;
+        actual = 0;
+        previo = -1;
+        fin = 0; //Bandera de finalización
 
+        //Ciclo para ingresar todo 
+        while (actual < 10 && fin == 0) {
+            //Aquí comienzo a solicitar que ingrese la cantidad de elementos que desee
+            String valor = JOptionPane.showInputDialog("Ingrese un elemento (ingrese -1 para terminar)");
 
+            if (valor == null || "".equals(valor)) {
+                JOptionPane.showMessageDialog(null, "Operación cancelada");
+                cancelar = true;
+                break;
+            }
 
+            int valorito = Integer.parseInt(valor);
 
+            if (valorito == -1) {
+                fin = 1;
+            } else {
+
+                nodo[actual][0] = valorito;
+                nodo[actual][1] = cabeza;
+                nodo[actual][2] = previo;
+
+                if (cabeza != -1) {
+                    nodo[cabeza][2] = actual;
+                }
+                cabeza = actual;
+                previo = actual;
+                actual = actual + 1;
+            }
+        }
+
+        if (!cancelar) {
+            //Ciclo para ir recorriendo la lista
+            String textofinal = "[";
+            actual = cabeza;
+            boolean primero = true; //bandera para que se muestre bonito
+
+            while (actual != -1) {
+                if (primero) {
+                    textofinal += nodo[actual][0];
+                    primero = false;
+                } else {
+                    textofinal += ", " + nodo[actual][0];
+                }
+                actual = nodo[actual][1]; //Me muevo al siguiente nodo
+            }
+            textofinal += "]"; //Cierro todo
+            JOptionPane.showMessageDialog(null, "La lista que se ingresó es:\n" + textofinal);
+        }
+    }
+
+    //Fucnión para generar la lista indexada
+    //Es basicamente la primera pero reacondicionada con le índice que considero innecesario
+    public void listaIndexada() {
+        boolean cancelar = false; //para manejar la cancelación de la operación
+        int n, i;
+        int[] lista = new int[10];
+        int[] indice = new int[10]; //Honestamente considero el índice como algo innecesario pero mi labor es no opinar
+        String entrada = JOptionPane.showInputDialog("Ingrese el número de elementos (el máximo es 10)");
+
+        if (entrada == null || "".equals(entrada)) {
+            JOptionPane.showMessageDialog(null, "Operación cancelada");
+            cancelar = true;
+        }
+
+        if (!cancelar) {
+            n = Integer.parseInt(entrada);
+
+            if (n > 10) {
+                JOptionPane.showMessageDialog(null, "Se ingresó un número mayor a 10, no se puede continuar con la acción");
+            } else {
+
+                for (i = 0; i < n; i++) {
+                    String valorcito = JOptionPane.showInputDialog("Ingrese el elemento " + i + ":");
+
+                    if (valorcito == null) {
+                        JOptionPane.showMessageDialog(null, "Operación cancelada");
+                        cancelar = true;
+                        break;
+                    }
+
+                    lista[i] = Integer.parseInt(valorcito);
+                    indice[i] = i;
+                }
+
+                if (!cancelar) {
+                    String textofinal = "[";
+                    for (i = 0; i < n; i++) {
+                        if (i == 0) {
+                            textofinal += "Posición: "+String.valueOf(indice[i]) + "=" + String.valueOf(lista[i]);
+                        } else {
+                            textofinal += ", " + "Posición: "+String.valueOf(indice[i]) + "=" + String.valueOf(lista[i]);
+                        }
+                    }
+                    textofinal += "]";
+                    JOptionPane.showMessageDialog(null, "La lista que se ingresó es:\n" + textofinal);
+                }
+
+            }
+        }
+
+    }
 }
--- SimulacionListas_MIA/src/main/java/com/mycompany/simulacionlistas_mia/gui.java
+++ SimulacionListas_MIA/src/main/java/com/mycompany/simulacionlistas_mia/gui.java
@@ -27,21 +27,38 @@ public class gui extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
         jButton5 = new javax.swing.JButton();
 
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBackground(new java.awt.Color(255, 255, 255));
+        setBackground(new java.awt.Color(153, 0, 153));
+
+        jPanel2.setBackground(new java.awt.Color(153, 204, 255));
+        jPanel2.setForeground(new java.awt.Color(255, 255, 204));
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("SELECCIONE EL TIPO DE LISTA");
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel1.setText(" SIMULACIÓN");
 
-        jButton1.setBackground(new java.awt.Color(204, 255, 255));
         jButton1.setText("Lista Contigua (Arreglo)");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -49,7 +66,6 @@ public class gui extends javax.swing.JFrame {
             }
         });
 
-        jButton2.setBackground(new java.awt.Color(204, 255, 255));
         jButton2.setText("Lista Ligada (Simple)");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -57,11 +73,27 @@ public class gui extends javax.swing.JFrame {
             }
         });
 
-        jButton3.setBackground(new java.awt.Color(204, 255, 255));
         jButton3.setText("Lista Doblemente Ligada");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
-        jButton4.setBackground(new java.awt.Color(204, 255, 255));
         jButton4.setText("Lista Indexada");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel2.setText(" Seleccione el tipo de lista:");
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel3.setText(" DE LISTAS");
 
         jButton5.setBackground(new java.awt.Color(204, 0, 0));
         jButton5.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
@@ -73,43 +105,62 @@ public class gui extends javax.swing.JFrame {
             }
         });
 
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 224, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton5)
+                    .addComponent(jButton4)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(jButton3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(0, 6, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel2))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jButton2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton5)))
+                .addContainerGap(10, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 190, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton4)
-                .addContainerGap(25, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addGap(75, 75, 75))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton5)
-                .addGap(294, 294, 294))
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2)
-                    .addComponent(jButton3)
-                    .addComponent(jButton4))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton5)
-                .addContainerGap(8, Short.MAX_VALUE))
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
@@ -129,6 +180,16 @@ public class gui extends javax.swing.JFrame {
         // TODO add your handling code here:
         simulacion.listaLigada();
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        simulacion.listaDoblementeLigada();
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        simulacion.listaIndexada();
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -173,5 +234,9 @@ public class gui extends javax.swing.JFrame {
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
